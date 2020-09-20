5517a60046a23e7bc9a0898b1c779368f62416d0
tyy <tyysun@outlook.com>
2020-09-20 23:56:23 +0800

    :lipstick: 完成页面部分搭建

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-import "./index.css"
+import "./index.scss"
 import { createWebHashHistory, createRouter } from "vue-router"
 // import HelloWorld from "./HelloWorld.vue"
-import Tyy from "./components/Tyy.vue"
+import Home from "./views/Home.vue"
+import Doc from "./views/Doc.vue"
+
 
 
 const history = createWebHashHistory()
@@ -11,7 +13,10 @@ const router = createRouter({
   history,
   routes: [
     {
-      path: '/', component: Tyy
+      path: '/', component: Home
+    },
+    {
+      path: '/doc', component: Doc
     }
   ]
 })
